45c3700f981c356120fbd8d31f0207d371d69b09
EnginKosure <engin.kosure@cronos.be>
2021-06-28 13:30:28 +0200

    add status route

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import json
+
 app = Flask(__name__)
+
+
+@app.route('/status')
+def status():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
