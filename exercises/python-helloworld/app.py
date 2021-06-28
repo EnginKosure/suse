b09f919ad928e9d1d29e8e17538e44701fdd3318
EnginKosure <engin.kosure@cronos.be>
2021-06-28 13:34:45 +0200

    add metrics code

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -11,6 +11,18 @@ def status():
         status=200,
         mimetype='application/json'
     )
+    return response
+
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0,
+                             "data": {"UserCount": 140, "UserCountActive": 23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 @app.route("/")
