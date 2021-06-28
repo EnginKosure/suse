e950caf4df0037641ac455334887e6c53512470c
EnginKosure <engin.kosure@cronos.be>
2021-06-28 16:15:04 +0200

    add logger

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ def status():
         status=200,
         mimetype='application/json'
     )
+    app.logger.info("Status request successfull")
     return response
 
 
@@ -22,13 +24,17 @@ def metrics():
         status=200,
         mimetype='application/json'
     )
+    app.logger.info('Metrics request successfull')
     return response
 
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
 
 if __name__ == "__main__":
+    # stream logs to app.log file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
